6cfa3a9bc60cde4a875868164ae2c1f76acb1522
Harsh Gupta <133102116+hg9336099029@users.noreply.github.com>
2026-01-22 22:58:10 +0530

    Delete .next/dev directory

--- .next/dev/static/chunks/app_page_jsx_8fe49dfa._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a3a127a8._.js",
-  "static/chunks/node_modules_d9a26933._.js"
-],
-    source: "dynamic"
-});
